082286d04f3150a41808eb0960f12b6de8696a70
Mark M <medvedevmark1999@gmail.com>
2022-10-29 15:57:24 +0300

    Ex_8

---
+++ Ex_8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int StartNumber = 1;
+
+while (StartNumber <= N)
+{ 
+    if (StartNumber % 2 == 0)
+    {
+        Console.WriteLine(StartNumber);
+    }
+    StartNumber += 1;
+}
