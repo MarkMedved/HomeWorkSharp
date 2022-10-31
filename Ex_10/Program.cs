971eed19ec95a861f987b3319e3915621ad3f251
Mark M <medvedevmark1999@gmail.com>
2022-10-31 17:56:23 +0300

    Ex_10

---
+++ Ex_10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число:");
+int num = Convert. ToInt32(Console.ReadLine());
+int x1 = num % 100;
+int x2 = x1 / 10;
+Console.WriteLine("Вторая цифра числа " + x2);
+
---
+++ Ex_15/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.IoInt32(Console.ReadLine());
+int x1 = num % 100;
+int x2 = x1 / 10;
+Console.WriteLine("Вторая строая цифра числа" + x2);
