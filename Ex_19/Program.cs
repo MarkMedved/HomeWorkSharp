5445489b38c7c395823445181bd8948cf35c0dfb
Mark M <medvedevmark1999@gmail.com>
2022-11-01 18:13:25 +0300

    Ex_19

---
+++ Ex_19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+        string number = Console.ReadLine();
+
+        void CheckingNumber(string number)
+        {
+            if (number[0] == number[4] || number[1] == number[3])
+            {
+                Console.WriteLine($"Число: {number} - является палиндромом.");
+            }
+            else Console.WriteLine($"Число: {number} - НЕ является палиндромом.");
+        }
+
+        if (number.Length == 5)
+        {
+            CheckingNumber(number);
+        }
+        else Console.WriteLine($"Ошибка ввода данных");
+
+
