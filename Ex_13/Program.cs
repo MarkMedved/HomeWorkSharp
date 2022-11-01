6f29112bfeb715072e2b848ec0e97442b8e6ccd6
Mark M <medvedevmark1999@gmail.com>
2022-11-01 15:05:28 +0300

    Ex_13

--- Ex_13/Program.cs
+++ Ex_13/Program.cs
@@ -1,10 +1,28 @@
-﻿Console.Clear();
-Console.WriteLine("Введите трехзначное число");
-    while(true)
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+// (Использовать только математические операции, нельзя использовать число как строку по типу number[2])
+// 20.10.2022 Sergey Dorofeev
+
+
+Console.Clear();
+
+int InputNum(string num)
+{
+    Console.Write(num);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numIn = InputNum($"Введите число: ");
+
+if (numIn / 100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (numIn > 1000)
     {
-        string input = Console.ReadLine();
-        if(!input.Equals("exit"))
-            Console.WriteLine("{0}->{1}",input, input[1]);
-        else
-            break;
+        numIn = numIn / 10;
     }
+    numIn = numIn % 10;
+    Console.WriteLine($"Третья цифра: {numIn}");
+}
