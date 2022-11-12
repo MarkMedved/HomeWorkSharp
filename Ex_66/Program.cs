9927c048f9ae19224e661d455ac1eea4e139c582
Mark M <medvedevmark1999@gmail.com>
2022-11-12 20:49:48 +0300

    Ex_66

---
+++ Ex_66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+int M = 5;
+int N = 100;
+int SumElem(int M, int N, int sum)
+{
+    if (M > N)
+    {
+        return sum;
+    }
+    sum = sum + (M++);
+    return SumElem(M, N, sum);
+}
+
+Console.WriteLine($"Сумма натуральных чисел от {M} до {N} равна: {SumElem(M, N, 0)}");
